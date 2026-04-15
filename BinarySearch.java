9b9c341fc3e35d47326f45c85ef78c85ba4d78ec
Saijogarao <141640422+Saijogarao@users.noreply.github.com>
2026-04-15 12:42:12 +0530

    Update array in BinarySearch for testing

--- BinarySearch.java
+++ BinarySearch.java
@@ -13,7 +13,7 @@ class GFG {
             if (arr[mid] < x)
                 low = mid + 1;
 
-            // If x is smaller, ignore right half
+           
             else
                 high = mid - 1;
         }
@@ -24,7 +24,7 @@ class GFG {
     }
 
     public static void main(String args[]) {
-        int arr[] = { 2, 3, 4, 10, 40 };
+        int arr[] = { 2, 3, 4, 7, 10, 40 };
         int x = 10;
         int result = binarySearch(arr, x);
         if (result == -1)
